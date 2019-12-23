c271fb2ad8e91012909c87a676a18d94c61b69b0
mohameDMEDO97 <mohamedtalaat0111790@gmail.com>
2019-12-23 17:31:11 +0200

    final2

--- Code/code/final.py
+++ Code/code/final.py
@@ -3,15 +3,13 @@ import numpy as np
 import os
 import glob
 import re
+import time
 from Line_segmentation import segment_paragragh
 from WordSegmentor import WordSegmentor
 from LineSegmentor import LineSegmentor
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
-import cv2
-import os
-import numpy as np
 from math import copysign, log10
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -26,8 +24,6 @@ from numpy import loadtxt
 from keras.models import Sequential
 from keras.layers import Dense
 import pandas
-from keras.models import Sequential
-from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
 from sklearn.model_selection import cross_val_score
@@ -36,7 +32,6 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.pipeline import Pipeline
 from numpy import array
 from numpy import argmax
-from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from keras.models import model_from_json
 from tempfile import TemporaryFile
@@ -44,6 +39,7 @@ from padding import pad
 
 
 input_path = 'C:/Users\medo\Desktop\Arabic-OCR\Code\input'
+output_path = 'C:/Users\medo\Desktop\Arabic-OCR\Code\output\\'
 
 numbers = re.compile(r'(\d+)')
 
@@ -97,16 +93,7 @@ def skew_correction(image):
     return rotated
 
 
-input_imgs = []
-for filename in sorted(glob.glob(os.path.join(input_path, '*.png')), key=numericalSort):
-    print(filename)
-    _imgs = cv2.imread(filename, 0)
-    input_imgs.append(_imgs)
-
-
-final_labels = {'alif': 'ا',
-                'baa': 'ب',
-                'taa': 'ت',
+final_labels = {'alif': 'ا', 'baa': 'ب', 'taa': 'ت',
                 'seh': 'ث',
                 'jiim': 'ج',
                 'haa': 'ح',
@@ -137,14 +124,12 @@ final_labels = {'alif': 'ا',
 
 
 # load labels
-
 Y = np.genfromtxt(
     'C:/Users\medo\Desktop\Arabic-OCR\Code\code\labels.txt', dtype='str')
 encoder = LabelEncoder()
 encoder.fit(Y)
 encoded_Y = encoder.transform(Y)
 dummy_y = np_utils.to_categorical(encoded_Y)
-
 
 # load model
 json_file = open(
@@ -159,25 +144,38 @@ print("Loaded model from disk")
 loaded_model.compile(loss='categorical_crossentropy',
                      optimizer='adam', metrics=['accuracy'])
 
+input_imgs = []
+for filename in sorted(glob.glob(os.path.join(input_path, '*.png')), key=numericalSort):
+    print(filename)
+    _imgs = cv2.imread(filename, 0)
+    input_imgs.append(_imgs)
 
-print(len(input_imgs))
+directory = output_path+'running_time.txt'
+
+out_time = open(directory, 'w+', encoding='utf-8')
+
+
 # time here
 for i in range(len(input_imgs)):
-    out = open('C:/Users\medo\Desktop\Arabic-OCR\Code\output\\test_' + str(i+1)+'.txt',
+    out = open('C:/Users\medo\Desktop\Arabic-OCR\Code\output\\text\\test_' + str(i+1)+'.txt',
                'w+', encoding='utf-8')
+    final_output = []
+    seconds1 = time.time()
     img = skew_correction(input_imgs[i])
     lines, lines_dil = LineSegmentor(img).segment_lines()
     words, length = WordSegmentor(lines, lines_dil).segment_words()
-    # array of arrays eac array contains chars imgs of word
+    # array of arrays each array contains chars imgs of word
     lt_img = segment_paragragh(lines, words)
     for i, word in enumerate(lt_img):
         for j, letter in enumerate(word):
             letter = pad(letter)
-            #gray = PreProcess(letter, 1)
-
             features = np.array(dct(letter).flatten()).reshape(1, 2500)
             res = loaded_model.predict(features)
             inverted = encoder.inverse_transform([argmax(res[0])])
-            print(final_labels[inverted[0]])
-            out.write(final_labels[inverted[0]])
-        out.write(' ')
+            final_output.append(final_labels[inverted[0]])
+        final_output.append(' ')
+    seconds2 = time.time()
+    final_time = seconds2-seconds1
+    out_time.write("%s\n" % final_time)
+    for char in final_output:
+        out.write(char)
